6bbe57688ccaa2dfd8b213e1be5560c1b178c9b2
baamkyu <baamkyu@gmail.com>
2022-11-07 17:56:07 +0900

    vuex

---
+++ Vue/05_vuex/todo-vuex-app/src/components/TodoForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input 
+    type="text"
+    v-model.trim="todoTitle"  
+    @keyup.enter="createTodo"
+    >
+    <!-- v-model.trim = 좌우 공백 제거 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoForm',
+  data() {
+    return {
+      todoTitle: null,
+    }
+  },
+  methods: {
+    createTodo() {
+      this.$store.dispatch('createTodo', this.todoTitle)
+      this.todoTitle = null
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
