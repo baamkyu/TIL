c550b8ea3e11c3b808b02a1aa1032a9cacb6abfe
baamkyu <baamkyu@gmail.com>
2022-12-11 10:25:45 +0900

    react-redux - connect & provider

--- 2학기 대비/React Redux/react-redux/src/containers/DisplayNumber.jsx
+++ 2학기 대비/React Redux/react-redux/src/containers/DisplayNumber.jsx
@@ -1,16 +1,28 @@
 import DisplayNumber from '../components/DisplayNumber'
-import React, {Component} from 'react'
-import store from '../store'
+import {connect} from 'react-redux'
 
-export default class extends Component {
-  state = {number: store.getState().number}
-  constructor(props) {
-    super(props)
-    store.subscribe(function() {
-      this.setState({number: store.getState().number})
-    }.bind(this))
-  }
-  render() {
-    return <DisplayNumber number={this.state.number}></DisplayNumber>
+function mapReduxStateToReactProps(state) {
+  return {
+    number: state.number
   }
 }
+function mapReduxDispatchToReactProps() {
+  return {}
+}
+export default connect(mapReduxStateToReactProps, mapReduxDispatchToReactProps)(DisplayNumber);
+
+// import React, {Component} from 'react'
+// import store from '../store'
+
+// export default class extends Component {
+//   state = {number: store.getState().number}
+//   constructor(props) {
+//     super(props)
+//     store.subscribe(function() {
+//       this.setState({number: store.getState().number})
+//     }.bind(this))
+//   }
+//   render() {
+//     return <DisplayNumber number={this.state.number}></DisplayNumber>
+//   }
+// }
