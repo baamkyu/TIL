8a70ce8a46a540830d73b65606cd1e5dc495b845
baamkyu <baamkyu@gmail.com>
2022-12-27 19:37:04 +0900

    js

---
+++ Programmers/JavaScript/Level2/04_올바른 괄호.js
@@ -0,0 +1,13 @@
+function solution(s){
+    let stackCount = 0;
+
+    for (let i = 0; i < s.length; i++) {
+      if (s[i] === '(') {
+        stackCount += 1
+      } else if (s[i] === ')') {
+        stackCount -= 1
+      }
+      if (stackCount < 0) return false;
+    }
+  return stackCount === 0 ? true : false;
+}
