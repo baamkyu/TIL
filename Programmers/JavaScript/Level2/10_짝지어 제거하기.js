a7cb42c613bc74254729b21ea57a2442deb2a68a
baamkyu <baamkyu@gmail.com>
2023-01-02 21:45:57 +0900

    js

---
+++ Programmers/JavaScript/Level2/10_짝지어 제거하기.js
@@ -0,0 +1,15 @@
+function solution(s){
+let arr = [];
+  for(let i=0; i < s.length; i++) {
+      if (arr[arr.length-1] != s[i]) {
+          arr.push(s[i]);
+      } else {
+          arr.pop();
+      }
+    }
+  if (arr.length > 0) {
+    return 0
+  } else {
+    return 1
+  }
+}
