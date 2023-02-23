8b1a51c5a654cf1ebcd14959db9bd76e79ce668f
dlaqjarb9 <dlaqjarb9@daum.net>
2023-02-23 17:47:16 +0900

    [TIL] NextJS practice

--- NextJS/02_nextjs-practice/pages/[meetupId]/index.js
+++ NextJS/02_nextjs-practice/pages/[meetupId]/index.js
@@ -2,13 +2,49 @@ import { Fragment } from "react";
 import MeetupDetail from '../../components/meetups/MeetupDetail'
 function MeetupDetails() {
   return (
-    <MeetupDetail 
-      image='https://upload.wikimedia.org/wikipedia/commons/f/f1/Action_off_toulon_4.jpg' 
-      title='A First Meetup' 
-      address="Some Street 5, Some City" 
-      description="The meetup description" 
+    <MeetupDetail
+      image='https://upload.wikimedia.org/wikipedia/commons/f/f1/Action_off_toulon_4.jpg'
+      title='A First Meetup'
+      address="Some Street 5, Some City"
+      description="The meetup description"
     />
   )
-}   
+}
 
+// 미트 업 데이터는 아주 자주 바뀌지 않음. 추가하는 기능만 있음 -> getStaticProps
+export async function getStaticPaths() {
+  return {
+    // 특정 path를 정의함 (모든 path가 아니라)
+    fallback: false,
+    paths: [
+      // 하드코드 했지만 현실 코드에서는 API사용해서
+      {
+        params: {
+          meetupId: 'm1'
+        }
+      },
+      {
+        params: {
+          meetupId: 'm2'
+        }
+      }
+    ]
+  }
+}
+export async function getStaticProps(context) {
+
+  const meetupId = context.params.meetupId
+
+  return {
+    props: {
+      meetupData: {
+        image: 'https://upload.wikimedia.org/wikipedia/commons/f/f1/Action_off_toulon_4.jpg',
+        id: meetupId,
+        title: 'A First Meetup',
+        address: "Some Street 5, Some City",
+        description: "The meetup description",
+      }
+    }
+  }
+}
 export default MeetupDetails;
--- NextJS/02_nextjs-practice/pages/index.js
+++ NextJS/02_nextjs-practice/pages/index.js
@@ -2,27 +2,44 @@ import MeetupList from '../components/meetups/MeetupList'
 import { useState, useEffect } from 'react'
 const DUMMY_MEETUPS = [
   {
-    id: '1',
+    id: 'm1',
     title: 'first',
     image: 'https://upload.wikimedia.org/wikipedia/commons/f/f1/Action_off_toulon_4.jpg',
     address: 'first, 1234 City',
     description: 'first meetup'
-  },  
+  },
   {
-    id: '2',
+    id: 'm2',
     title: 'second',
     image: 'https://upload.wikimedia.org/wikipedia/commons/f/f1/Action_off_toulon_4.jpg',
     address: 'second, 2345 City',
     description: 'second meetup'
   },
 ]
-function HomePage() {
-  const [ loadedMeetups, setLoadedMeetups ] = useState([])
-  useEffect(() => {
-    setLoadedMeetups(DUMMY_MEETUPS)
-  }, [])
+function HomePage(props) {
   return (
-  <MeetupList meetups={DUMMY_MEETUPS} />
+    <MeetupList meetups={props.meetups} />
   )
-}   
+}
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+//   // fetch data from an API
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS
+//     }
+//   }
+// }
+
+export async function getStaticProps() {
+  // fetch data from an API
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS
+    },
+    revalidate: 1
+  }
+}
+
 export default HomePage;
