583a2c6d9a083ad024f835433fb1b9bb80b900b9
baamkyu <baamkyu@gmail.com>
2023-04-03 20:49:38 +0900

    알고리즘 풀이

--- Algorithm/Python/SWEA/D2/4865_글자수.py
+++ Algorithm/Python/SWEA/D2/4865_글자수.py
@@ -3,7 +3,6 @@ for tc in range(1, T+1):
   set_word = list(set(input())) # 첫번째 인풋을 중복제거해줌
   check = [0]*len(set_word)
   input_word = list(input())   # 두번째 인풋
-  print(input_word)
   for i in range(len(set_word)):
     for j in input_word:
       if j == set_word[i]:
