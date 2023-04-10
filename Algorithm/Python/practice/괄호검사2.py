f73a60f46d0f023cacc4da9f4c8f7bc3679fbc99
dlaqjarb9 <dlaqjarb9@daum.net>
2023-04-10 16:38:23 +0900

    알고리즘 풀이

---
+++ Algorithm/Python/practice/괄호검사2.py
@@ -0,0 +1,18 @@
+T = int(input())
+for tc in range(1, T+1):
+    input = input()
+    stack = []
+    is_perfect = 0  # 올바른 괄호인지 확인
+    for i in input:
+        if i == '(':
+            stack.append(i)
+        else:
+            if stack:
+                stack.pop()
+            else:
+                is_perfect = 0
+    if stack:
+        pass
+    else:
+        is_perfect = 1
+print(f'#{tc} {is_perfect}')
