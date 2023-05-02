775223822a3415c97a3fc89d42de1c77e9de87ad
baamkyu <baamkyu@gmail.com>
2023-05-02 22:09:32 +0900

    python algorithm

---
+++ Algorithm/Python/SWEA/D2/5102_노드의거리.py
@@ -0,0 +1,8 @@
+T = int(input())
+for tc in range(1, T+1):
+    V, E = map(int, input().split())
+    adj_list = [[] for _ in range(V+1)]
+    for _ in range(E):
+        start, end = map(int, input().split())
+        adj_list[start].append(end)
+        # 이어서 ㄱㄱ
