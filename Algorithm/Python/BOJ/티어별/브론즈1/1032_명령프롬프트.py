1ed1b9abb744524b1997b893d4c1d8635e752b72
baamkyu <baamkyu@gmail.com>
2023-06-14 22:21:43 +0900

    BOJ 브론즈1

--- Algorithm/Python/BOJ/티어별/브론즈1/1032_명령프롬프트.py
+++ Algorithm/Python/BOJ/티어별/브론즈1/1032_명령프롬프트.py
@@ -1,16 +1,11 @@
 N = int(input())
-input_list = []
-for _ in range(N):
-    input_list.append(input())
+target = list(input())          # 첫번째 리스트 입력
 
-first = input_list[0]
-
-# listnum : 몇번째 리스트 ?
-# letternum : 몇번째 글자 ?
-for listnum in range(1, N):
-    for letternum in range(len(first)):
-        if input_list[listnum][letternum] == first[letternum]:
+for _ in range(N-1):
+    compare = list(input())     # 비교할 리스트 입력
+    for i in range(len(target)):
+        if target[i] == compare[i]:
             pass
-        elif input_list[listnum][letternum] != first[letternum]:
-            first[letternum] = "?"
-        
+        else:       # 다른 글자이면 ?로 변환
+            target[i] = '?'
+print(''.join(target))
