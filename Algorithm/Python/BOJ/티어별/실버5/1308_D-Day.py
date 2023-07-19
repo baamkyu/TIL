ae65730f2f9c340107621196a47ab05a1cbb4f40
baamkyu <baamkyu@gmail.com>
2023-07-19 20:15:58 +0900

    BOJ 티어별 - 실버5

--- Algorithm/Python/BOJ/티어별/실버5/1308_D-Day.py
+++ Algorithm/Python/BOJ/티어별/실버5/1308_D-Day.py
@@ -1,2 +1,16 @@
 # 현재의 날짜, 캠프의 종료일이 주어지고 현재 기준으로 종료까지 며칠 남았는지 출력하라.
 
+import datetime
+start = list(map(int, input().split()))
+end = list(map(int, input().split()))
+
+start_day = datetime.date(start[0], start[1], start[2])
+start_after_1000 = datetime.date(start[0] + 1000, start[1], start[2])
+end_day = datetime.date(end[0], end[1], end[2])
+
+result_1000 = start_after_1000 - start_day
+result = end_day - start_day
+if result < result_1000:
+  print("D-{}".format(result.days))
+else:
+  print("gg")
