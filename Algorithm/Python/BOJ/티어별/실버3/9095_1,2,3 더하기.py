a35d4683203854f2af630cb490fb13ca1059230f
baamkyu <baamkyu@gmail.com>
2023-07-25 22:20:53 +0900

    BOJ: DP

--- Algorithm/Python/BOJ/티어별/실버3/9095_1,2,3 더하기.py
+++ Algorithm/Python/BOJ/티어별/실버3/9095_1,2,3 더하기.py
@@ -1,14 +1,17 @@
-T = int(input()) #테스트 케이스 개수
+import sys
+input = sys.stdin.readline
 
-N = [] #n을 저장할 list
+def sol(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    elif n == 3:
+        return 4
+    else:
+        return sol(n-3) + sol(n-2) + sol(n-1)
 
-for i in range(T):
-    N.append(int(input()))
-
-dp = [0,1,2,4] #1,2,3에 대한 값 : 초깃값
-
-for i in range(4, max(N)+1): #4부터 N에 저장된 최댓값까지
-    dp.append(dp[i-1]+dp[i-2]+dp[i-3]) 
-
-for n in N:
-    print(dp[n])
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    print(sol(n))
